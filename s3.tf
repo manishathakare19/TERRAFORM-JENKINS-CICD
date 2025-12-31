006be0b2451561a2cd9a2570ab85b2e1a3915ff9
Manisha Mahesh Thakare <132184963+manishathakare19@users.noreply.github.com>
2025-12-31 18:36:13 +0530

    i

--- s3.tf
+++ s3.tf
@@ -1,9 +1,94 @@
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = var.bucket_name
+# Create S3 bucket
+resource "aws_s3_bucket" "mybucket" {
+  bucket        = var.bucketname
+  force_destroy = true
+}
 
-  versioning {
-    enabled = true
+# Enable website hosting
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  index_document {
+    suffix = "index.html"
   }
 
-  acl = var.acl
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [
+    aws_s3_bucket.mybucket
+  ]
 }
+
+# Public read bucket policy
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.mybucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [
+    aws_s3_bucket.mybucket,
+    aws_s3_bucket_website_configuration.website
+  ]
+}
+
+# Upload website files
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  acl          = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_policy.public_read
+  ]
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
+  acl          = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_policy.public_read
+  ]
+}
+
+resource "aws_s3_object" "style" {
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "style.css"
+  source       = "style.css"
+  content_type = "text/css"
+  acl          = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_policy.public_read
+  ]
+}
+
+resource "aws_s3_object" "script" {
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "script.js"
+  source       = "script.js"
+  content_type = "text/javascript"
+  acl          = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_policy.public_read
+  ]
+}
+
