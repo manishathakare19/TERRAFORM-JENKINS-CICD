f5f275ea8ce2cfbb0b3c63dad8f2ebef09695ab7
Manisha Mahesh Thakare <132184963+manishathakare19@users.noreply.github.com>
2025-12-31 18:36:47 +0530

    i

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
-resource "aws_instance" "Ajay" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  user_data              = base64encode(file("website.sh"))
-  tags = {
-    Name = "Aj-EC2"
-  }
-}
-
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22 and 443"
+  vpc_id      = var.vpc_id
 
   ingress {
     description      = "ssh access"
@@ -24,38 +14,55 @@ resource "aws_security_group" "ec2_security_group" {
 
   ingress {
     from_port   = 0
-    to_port     = 0    # Allow all ports
-    protocol    = "-1" # All protocols
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "https"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
-    description      = "http"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
     Name = "Aj_sg"
   }
+}
+
+resource "aws_instance" "Ajay" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  user_data              = base64encode(file("website.sh"))
+  tags = {
+    Name = "Aj-EC2"
+  }
+
+  depends_on = [
+    aws_security_group.ec2_security_group,
+    aws_s3_bucket.mybucket,
+    aws_s3_bucket_website_configuration.website
+  ]
 }
